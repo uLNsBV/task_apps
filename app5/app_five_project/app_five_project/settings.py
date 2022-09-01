cfa607767849f7b9c14908b94422616f17071ed9
uLNsBV <ulan.eshbaev571@gmail.com>
2022-09-01 08:48:34 +0600

    secret key hidden

--- app5/app_five_project/app_five_project/settings.py
+++ app5/app_five_project/app_five_project/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from distutils.command.config import config
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dt+9jr2_z!%lkh&hd48=pjy!jsr!r27scjx)mf$gn4oy3hcz%1'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
